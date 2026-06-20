fc29df726b08ac03330e0af3e53d1f1ed91e23fb
metspy9069 <nagasubhash55@gmail.com>
2026-06-20 15:05:13 +0530

    Replaced Exposed Service role key

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,5 +1,7 @@
 class AppConstants {
-  static const String supabaseUrl = 'https://qkqekwupkghwduxoyzvc.supabase.co';
-  // Replacing anon key with service role key to bypass RLS restriction
-  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrcWVrd3Vwa2dod2R1eG95enZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5MzA0NTMsImV4cCI6MjA5NzUwNjQ1M30.JJeVZE23WpgRevJFqqcnG2B93HzKB7FayCDNBQTno0g';
+  static const String supabaseUrl =
+      'https://qkqekwupkghwduxoyzvc.supabase.co';
+
+  static const String supabaseAnonKey =
+      'sb_publishable_wCR4gTG24G4eGtf1dfc91A_cY83AvLD';
 }
